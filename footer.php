16dde507af89bc312f73a918a58a90e79bc8db26
Marko Idzan <42409619+idzan@users.noreply.github.com>
2019-06-25 23:12:35 +0200

    Removed code which can be detectable with company

--- footer.php
+++ footer.php
@@ -10,13 +10,6 @@ global $flatsome_opt;
 
 </main><!-- #main -->
 
-<!-- Your customer chat code -->
-<div class="fb-customerchat"
-  page_id="505416389492806"
-  theme_color="#D8CB0F"
-  logged_in_greeting="Pozdrav! Dobro došli na maskice.hr web stranicu! Kako Vam možemo pomoći?"
-  logged_out_greeting="Pozdrav! Dobro došli na maskice.hr web stranicu! Kako Vam možemo pomoći?">
-</div>
 
 <footer id="footer" class="footer-wrapper">
 
